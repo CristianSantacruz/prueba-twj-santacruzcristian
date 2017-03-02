9e4e26aa6c385b31ab412edb249486fd55ba59e6
CristianSantacruz <santacruz-guarquila@hotmail.com>
2017-03-02 11:20:56 -0500

    RutasController

--- Prueba/api/controllers/RutasController.js
+++ Prueba/api/controllers/RutasController.js
@@ -6,6 +6,86 @@
  */
 
 module.exports = {
-	
-};
+  home: function (req, res) {
+    return res.view('Vistas/home', {
+      title: 'Inicio'
+    })
+  },
+  crearCelular: function (req, res) {
+    return res.view('Vistas/Celular/crearCelular', {
+      title: 'Crear Celular'
+    })
+  },
+  editarCelular: function (req, res) {
+    var parametros = req.allParams();
+    console.log(parametros);
+    if (parametros.id) {
+      Celular.findOne({
+        id: parametros.id
+      }).exec(function (error, celularEncontrado) {
+        if (error) return res.serverError();
+        return res.view('Vistas/Celular/editarCelular', {
+          title: 'Editar Celular - ' + celularEncontrado.nombre,
+          celular: celularEncontrado
+        })
+      });
+
+    } else {
+      return res.view('error', {
+        title: 'Error',
+        error: {
+          descripcion: 'No existe el ID'
+        }
+      });
+    }
+  },
+  listarCelular: function (req, res) {
+    Celular.find().exec(function (error, celularesEncontrados) {
+      if (error) return res.serverError();
+      sails.log.info(celularesEncontrados);
+      return res.view('Vistas/Celular/listarCelular', {
+        title: 'Lista de Celulares',
+        celulares: celularesEncontrados
+      })
+    });
+  },
+  crearAplicacion: function (req, res) {
+    return res.view('Vistas/Aplicacion/crearAplicacion', {
+      title: 'Crear Aplicacion'
+    })
+  },
+  editarAplicacion: function (req, res) {
+    var parametros = req.allParams();
+    console.log(parametros);
+    if (parametros.id) {
+      Aplicacion.findOne({
+        id: parametros.id
+      }).exec(function (error, aplicacionEncontrada) {
+        if (error) return res.serverError();
+        return res.view('Vistas/Aplicacion/editarAplicacion', {
+          title: 'Editar Aplicacion - ' + celularEncontrado.nombre,
+          aplicacion: aplicacionEncontrada
+        })
+      });
+
+    } else {
+      return res.view('error', {
+        title: 'Error',
+        error: {
+          descripcion: 'No existe el ID'
+        }
+      });
+    }
+  },
+  listarAplicacion: function (req, res) {
+    Aplicacion.find().exec(function (error, aplicacionesEncontradas) {
+      if (error) return res.serverError();
+      sails.log.info(aplicacionesEncontradas);
+      return res.view('Vistas/Aplicacion/listarAplicacion', {
+        title: 'Lista de Aplicaciones',
+        celulares: aplicacionesEncontradas
+      })
+    });
+  }
+  };
 
